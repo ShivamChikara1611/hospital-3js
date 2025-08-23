e533a4f104da0bfeef29932d92b7e62e0b0b2bb2
shivam chikara <s.chikara6885@gmail.com>
2025-08-24 01:59:22 +0530

    changed the url of skincancer-api

--- frontend/src/pages/SkinCancerDetection.jsx
+++ frontend/src/pages/SkinCancerDetection.jsx
@@ -52,7 +52,7 @@ const SkinCancerDetection = () => {
         formData.append("file", file);
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/predict", {
+            const response = await fetch(process.env.SKIN_CANCER_DETECTION_URL, {
                 method: "POST",
                 body: formData,
             });
