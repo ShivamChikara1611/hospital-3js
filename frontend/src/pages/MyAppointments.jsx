ad71f09ba0bf3be384a2338295fbb85920b7140a
shivam chikara <s.chikara6885@gmail.com>
2025-08-27 03:01:55 +0530

    updated the my appointment section

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -134,52 +134,60 @@ const MyAppointments = () => {
         }
     })
 
-    const [doctorsToShow, setDoctorsToShow] = useState(window.innerWidth < 720 ? 5 : 5);
+    const [doctorsToShow, setDoctorsToShow] = useState(window.innerWidth < 720 ? 4 : 6);
 
     const loadMoreDoctors = () => {
         setDoctorsToShow(prevCount => prevCount + (window.innerWidth < 720 ? 2 : 3)); // Load more doctors
     }
 
     return (
-        <div className='mb-[150px]'>
-            <h1 className='mt-[80px] text-4xl font-bold text-center text-gray-800'>My <span className='text-primary'>Appointments.</span></h1>
+        <div className='my-10 mx-2 md:mx-0'>
+            <h1 className='text-2xl text-center tracking-wide text-gray-200'>My Appointments</h1>
 
-            <div className='mt-[60px] max-w-[1000px] mx-auto'>
+            <div className='mt-6 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-[350px] mx-auto md:max-w-full'>
                 {
                     appointments.slice(0, doctorsToShow).map((item, index) => (
-                        <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 bg-third mb-5 p-5 rounded-lg' key={index}>
-                            <div>
-                                <img className='w-32 bg-primary rounded' src={item.docData.image} alt="" />
+                        <div className='bg-white/10 mb-2 md:mb-0 p-2 lg:p-3'>
+
+                            {/* image and details */}
+                            <div className='grid grid-cols-[1fr_3fr] gap-2' key={index}>
+                                <div>
+                                    <img className='w-32 bg-white/30' src={item.docData.image} alt="" />
+                                </div>
+                                <div className='flex-1 text-gray-300'>
+                                    <p className='text-gray-200 text-lg md:text-xl font-medium'>
+                                    {item.docData.name.trim().split(' ').slice(0, 1)} {item.docData.name.trim().split(' ').slice(1, 2)} {item.docData.name.trim().split(' ').slice(2, 3)}
+                                    </p>
+                                    <p className='text-xs'>{item.docData.speciality}</p>
+                                    <p className='text-xs'><span className='text-gray-400'>Date & Time:</span> {slotDateFormat(item.slotDate)} | {item.slotTime}</p>
+                                </div>
                             </div>
-                            <div className='flex-1 text-sm text-gray-600'>
-                            <p className='text-gray-800 text-lg font-medium'>
-                                {item.docData.name.split(' ').slice(0, 1).join(' ')} <span className='text-primary'>
-                                    {item.docData.name.split(' ').slice(1, 2).join(' ')}
-                                </span> {item.docData.name.split(' ').slice(2, 3).join(' ')}
-                            </p>
-                                <p>{item.docData.speciality}</p>
-                                <p className='text-zinc-500 font-medium mt-1'>Address:</p>
-                                <p className='text-xs'>{item.docData.address.line1}</p>
-                                <p className='text-xs'>{item.docData.address.line2}</p>
-                                <p className='text-xs mt-1'><span className='text-sm text-neutral-500 font-medium'>Date & Time:</span> {slotDateFormat(item.slotDate)} | {item.slotTime}</p>
-                            </div>
-                            <div></div>
-                            <div className='flex flex-col gap-2 justify-end'>
-                                {!item.cancelled && item.payment && !item.isCompleted && <button className='text-sm text-gray-800 border-gray-800 text-center sm:min-w-48 py-2 border rounded hover:bg-green-500 hover:border-transparent hover:text-black transition-all duration-200'>Paid</button>}
-                                {!item.cancelled && !item.payment && !item.isCompleted && <button onClick={() => appointmentRazorpay(item._id)} className='text-sm text-gray-800 text-center border-gray-800 sm:min-w-48 py-2 border rounded hover:bg-primary hover:border-transparent hover:text-white transition-all duration-200'>Pay Online</button>}
-                                {!item.cancelled && !item.isCompleted && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-gray-800 text-center border-gray-800 sm:min-w-48 py-2 border rounded hover:bg-red-500 hover:border-transparent hover:text-white transition-all duration-200'>Cancel appointment</button>}
-                                {item.cancelled && !item.isCompleted && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-center sm:min-w-48 py-2 border border-red-500 bg-red-500 text-white rounded'>Appointment cancelled</button>}
+
+                            {/* buttons div */}
+                            <div className='flex mt-2 gap-1 md:gap-2 justify-center w-full'>
+                                {!item.cancelled && item.payment && !item.isCompleted && <button className='text-sm text-gray-300 bg-green-600 text-center border py-2 border-green-600 font-light tracking-wide w-full'>Paid</button>}
+
+                                {/* pay online */}
+                                {!item.cancelled && !item.payment && !item.isCompleted && <button onClick={() => appointmentRazorpay(item._id)} className='text-sm text-green-600 py-2 text-center border border-green-600 font-light tracking-wide hover:text-gray-300 hover:bg-green-600 transition-all duration-200 w-full'>Pay Online</button>}
+
+                                {/* cancel appointment */}
+                                {!item.cancelled && !item.isCompleted && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-red-600 text-center border py-2 border-red-600 font-light tracking-wide hover:text-gray-300 hover:bg-red-600 transition-all duration-200 w-full'>Cancel</button>}
+
+                                {/* appointment cancelled and completed*/}
+                                {item.cancelled && !item.isCompleted && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-gray-300 bg-red-600 text-center border py-2 border-red-600 font-light tracking-wide w-full'>Appointment cancelled</button>}
                                 {
-                                    item.isCompleted && <button className='text-sm text-white text-center sm:min-w-48 py-2 border border-green-500 bg-green-500 rounded'>Completed</button>
+                                    item.isCompleted && <button className='text-sm text-gray-300 bg-primary text-center border py-2 border-primary font-light tracking-wide w-full'>Completed</button>
                                 }
                             </div>
                         </div>
                     ))
                 }
             </div>
-            <div className='flex justify-center w-full mt-10'>
+
+            {/* more button */}
+            <div className='flex justify-center w-full mt-6 mb-16'>
                 {doctorsToShow && (
-                    <button onClick={loadMoreDoctors} className='text-white border-2 border-primary text-lg mt-6 bg-primary rounded-full py-2 hover:bg-transparent hover:text-primary hover:border-2 hover:border-primary hover:scale-105 transition-all duration-300 px-10'>
+                    <button onClick={loadMoreDoctors} className='bg-white/5 backdrop-blur-md text-gray-200 px-8 py-2 rounded-full hover:bg-primary/70 transition-all duration-300 font-light tracking-wider'>
                         More
                     </button>
                 )}
