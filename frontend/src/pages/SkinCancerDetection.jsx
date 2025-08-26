94bed1275ee0ee01b90050802312ed5f6f9e0f32
shivam chikara <s.chikara6885@gmail.com>
2025-08-27 04:09:22 +0530

    updated the skin cancer page

--- frontend/src/pages/SkinCancerDetection.jsx
+++ frontend/src/pages/SkinCancerDetection.jsx
@@ -72,8 +72,8 @@ const SkinCancerDetection = () => {
     };
 
     return (
-        <div className="mt-[50px] text-gray-800 flex flex-col w-full mb-[150px]">
-            <h1 className="text-4xl sm:text-6xl mb-[50px] font-semibold text-center"><span className="text-primary">Conquering</span> Skin Cancer Together
+        <div className="my-10 px-2 md:px-0 text-gray-200 flex flex-col w-full">
+            <h1 className="text-2xl font-light tracking-wide mb-6 text-center">Conquering Skin Cancer Together
             </h1>
 
             <article>
@@ -88,7 +88,7 @@ const SkinCancerDetection = () => {
                                 key={index}
                                 src={src}
                                 alt={`Slide ${index + 1}`}
-                                className="w-full h-full object-contain flex-shrink-0 mb-12 md:mb-0"
+                                className="w-full h-full object-contain flex-shrink-0 mb-5 md:mb-0"
                             />
                         ))}
                     </div>
@@ -96,22 +96,22 @@ const SkinCancerDetection = () => {
 
                 {/* Detail Description */}
                 <h3 className="text-primary font-bold text-lg">A Message of Hope & Strength</h3>
-                <p className="text-sm text-gray-500 font-light">At the heart of every challenge lies the opportunity for triumph. Whether you are beginning your journey or have been battling skin cancer for some time, remember that early detection and proactive care are powerful tools. With cutting-edge technology and compassionate support, you are not alone on this journey.</p>
+                <p className="text-sm text-gray-400 font-light">At the heart of every challenge lies the opportunity for triumph. Whether you are beginning your journey or have been battling skin cancer for some time, remember that early detection and proactive care are powerful tools. With cutting-edge technology and compassionate support, you are not alone on this journey.</p>
 
                 <br />
 
                 <h3 className="text-primary font-bold text-lg">What Is Skin Cancer?</h3>
-                <p className="text-sm text-gray-500 font-light">Skin cancer affects millions, but knowledge is your first line of defense. By understanding the warning signs and risk factors, you empower yourself to take timely action. Our advanced detection tools are designed to provide you with the insights needed for early diagnosis and intervention.</p>
+                <p className="text-sm text-gray-400 font-light">Skin cancer affects millions, but knowledge is your first line of defense. By understanding the warning signs and risk factors, you empower yourself to take timely action. Our advanced detection tools are designed to provide you with the insights needed for early diagnosis and intervention.</p>
 
                 <br />
 
                 <h3 className="text-primary font-bold text-lg">Our Innovative Approach</h3>
-                <p className="text-sm text-gray-500 font-light">Imagine a future where technology meets compassion—a future where smart tools help in detecting early signs of skin cancer, giving you the opportunity to act before it’s too late. With our AI-driven detection system, you gain clarity, support, and the chance to address skin cancer at its earliest stages.</p>
+                <p className="text-sm text-gray-400 font-light">Imagine a future where technology meets compassion—a future where smart tools help in detecting early signs of skin cancer, giving you the opportunity to act before it’s too late. With our AI-driven detection system, you gain clarity, support, and the chance to address skin cancer at its earliest stages.</p>
 
                 <br />
 
                 <h3 className="text-primary font-bold text-lg">Steps to Beat Skin Cancer:</h3>
-                <ol className="text-sm text-gray-500 font-light list-decimal list-inside">
+                <ol className="text-sm text-gray-400 font-light list-decimal list-inside">
                     <li>Early Detection: Regular screenings can save lives. Our tool helps identify potential issues early, giving you a head start in treatment.</li>
                     <li>Informed Decisions: With accurate results and detailed insights, you can work closely with your healthcare provider to choose the best path forward.
                     </li>
@@ -124,40 +124,42 @@ const SkinCancerDetection = () => {
                 <br />
 
                 <h3 className="text-primary font-bold text-lg">You Are Stronger Than You Think</h3>
-                <p className="text-sm text-gray-500 font-light">Every step you take towards understanding and combating skin cancer is a step towards a healthier, brighter future. Embrace the spirit of resilience and know that every day brings new hope. With the right tools, information, and support, you can beat skin cancer and reclaim your life.
+                <p className="text-sm text-gray-400 font-light">Every step you take towards understanding and combating skin cancer is a step towards a healthier, brighter future. Embrace the spirit of resilience and know that every day brings new hope. With the right tools, information, and support, you can beat skin cancer and reclaim your life.
                 </p>
             </article>
 
 
 
-            <div className="bg-primary rounded-lg mt-[100px] p-5 max-w-[400px] mx-auto">
-                <h3 className="text-center text-xl font-semibold text-white">Check Here</h3>
+            <div className="bg-white/10 rounded-lg mt-16 px-2 py-4 max-w-[400px] mx-auto">
+                {showPreview ? <h3 className="text-center text-xl font-light tracking-wider text-gray-200">Result</h3> : <h3 className="text-center text-xl font-light tracking-wider text-gray-200">Check Here</h3>}
 
-                <div className="md:flex gap-5 items-center w-full justify-center w-full mt-5">
+                <div className="w-full mt-5">
                     {/* File Upload */}
-                    <input
+                    {!showPreview &&
+                    (<input
                         type="file"
                         accept=".jpg, .jpeg"
                         onChange={handleFileChange}
-                        className="p-1.5 rounded bg-third cursor-pointer w-full mb-2 md:mb-0 text-sm"
-                    />
+                        className="p-1.5 rounded bg-white/20 cursor-pointer w-full mb-4 text-sm text-gray-400"
+                    />)}
 
                     {/* Predict Button */}
-                    <div>
+                    {!showPreview && (
+                    <div className="flex justify-center w-full mt-6">
                         <button
                             onClick={handlePredict}
                             disabled={loading}
-                            className="px-5 py-2 bg-red-500 text-white rounded text-sm hover:bg-red-600 transition duration-300"
+                            className="px-8 py-3 bg-red-600 text-gray-200 rounded-full text-sm cursor-pointer"
                         >
                             {loading ? "Detecting..." : "Detect"}
                         </button>
-                    </div>
+                    </div>)
+                    }
                 </div>
 
                 {/* Preview Uploaded Image */}
                 {showPreview && previewUrl && (
-                    <div className="mt-5 w-full">
-                        <h3 className="text-md text-gray-800 mb-3">Result</h3>
+                    <div>
                         <img
                             src={previewUrl}
                             alt="Uploaded Preview"
@@ -168,14 +170,20 @@ const SkinCancerDetection = () => {
 
 
                 {/* Error Message */}
-                {error && <p className="text-sm text-red-500 mt-5">{error}</p>}
+                {error && <p className="text-md mt-8 text-center tracking-wide text-red-600">{error}</p>}
 
 
                 {/* Prediction Result */}
-                {prediction && (
+                {prediction && showPreview && (
                     <div className="mt-5">
                         <p>Status: {prediction.prediction === "Malignant" ? 'True' : 'False'}</p>
                         <p>Accuracy: {prediction.confidence * 100}%</p>
+                    </div>
+                )}
+
+                {showPreview && (
+                    <div className="flex justify-center w-full mt-6">
+                        <button className="px-8 py-3 bg-white/20 text-gray-200 rounded-full text-sm cursor-pointer" onClick={() => setShowPreview(!showPreview)}>Retake Photo</button>
                     </div>
                 )}
             </div>
